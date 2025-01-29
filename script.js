22ebb68e0d8f59b4095012f002e699b25c3bd38d
lmorgan2398 <lmorgan2398@gmail.com>
2025-01-28 21:28:55 -0600

    Reformat files for multiple iterations of columns
    Added more specific classes and IDs to several elements,
    applied appropriate selectors in script, and then adjusted
    current functions to work separately on each column to allow
    for several different colors to be selected at once (up to 7)

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 // Create variable for color selected via color picker
 
-let currentColor = '#FFFFFF';
-let colorPicker = document.getElementById('color');
+let currentColor1 = '#FFFFFF';
+let currentColor2 = '#FFFFFF';
+let currentColor3 = '#FFFFFF';
+let currentColor4 = '#FFFFFF';
+let currentColor5 = '#FFFFFF';
+let currentColor6 = '#FFFFFF';
+let currentColor7 = '#FFFFFF';
+
+let colorPicker1 = document.getElementById('color1');
+let colorPicker2 = document.getElementById('color2');
+let colorPicker3 = document.getElementById('color3');
+let colorPicker4 = document.getElementById('color4');
+let colorPicker5 = document.getElementById('color5');
+let colorPicker6 = document.getElementById('color6');
+let colorPicker7 = document.getElementById('color7');
+
 
 // Convert variable value to HSB
 // Split HSB to separate Hue, Saturation, and Brightness values
@@ -9,7 +23,6 @@ let colorPicker = document.getElementById('color');
 
 function hexToHue(hex) {
     hex = hex.substr(1);
-    console.log(hex);
     let r = parseInt(hex.substring(0, 2), 16);
     let g = parseInt(hex.substring(2, 4), 16);
     let b = parseInt(hex.substring(4, 6), 16);
@@ -56,7 +69,7 @@ function hsbToHex(h, s, b) {
 // Iterate over Saturation and Brightness values per hue to create different shades
 // Apply different shades to corresponding divs
 
-function shadeHue(hue) {
+function shadeHue(hue, column) {
   let sList = [10, 30, 50, 70, 90, 100, 100, 100, 100]
   let bList = [100, 100, 100, 100, 90, 70, 50, 30, 10]
   
@@ -65,20 +78,94 @@ function shadeHue(hue) {
       let s = sList[i];
       let b = bList[i];
       let newHex = hsbToHex(h, s, b);
-      document.querySelector(`.shade${i + 1}`).style.backgroundColor = newHex;
-      document.querySelector(`.shade${i + 1}`).textContent = newHex.toUpperCase();
+      document.querySelector(`.column${column} .shade${i + 1}`).style.backgroundColor = newHex;
+      document.querySelector(`.column${column} .shade${i + 1}`).textContent = newHex.toUpperCase();
     };
   return;
 }
 
 // Create input for changing selected color
 
-colorPicker.addEventListener('input', () => {
-    currentColor = document.getElementById('color').value;
-    document.querySelector('label').style.backgroundColor = currentColor;    
+colorPicker1.addEventListener('input', () => {
+    currentColor1 = document.getElementById('color1').value;
+    document.querySelector('.column1 label').style.backgroundColor = currentColor1;    
 });
 
-colorPicker.addEventListener('change', () => {
-  let hue = hexToHue(currentColor);
-  shadeHue(hue);
+colorPicker1.addEventListener('change', () => {
+  let hue = hexToHue(currentColor1);
+  shadeHue(hue, '1');
 });
+
+// Repeat process for each color picker
+
+colorPicker2.addEventListener('input', () => {
+  currentColor2 = document.getElementById('color2').value;
+  document.querySelector('.column2 label').style.backgroundColor = currentColor2;    
+});
+
+colorPicker2.addEventListener('change', () => {
+let hue = hexToHue(currentColor2);
+shadeHue(hue, '2');
+});
+
+
+
+colorPicker3.addEventListener('input', () => {
+  currentColor3 = document.getElementById('color3').value;
+  document.querySelector('.column3 label').style.backgroundColor = currentColor3;    
+});
+
+colorPicker3.addEventListener('change', () => {
+let hue = hexToHue(currentColor3);
+shadeHue(hue, '3');
+});
+
+
+
+colorPicker4.addEventListener('input', () => {
+  currentColor4 = document.getElementById('color4').value;
+  document.querySelector('.column4 label').style.backgroundColor = currentColor4;    
+});
+
+colorPicker4.addEventListener('change', () => {
+let hue = hexToHue(currentColor4);
+shadeHue(hue, '4');
+});
+
+
+
+colorPicker5.addEventListener('input', () => {
+  currentColor5 = document.getElementById('color5').value;
+  document.querySelector('.column5 label').style.backgroundColor = currentColor5;    
+});
+
+colorPicker5.addEventListener('change', () => {
+let hue = hexToHue(currentColor5);
+shadeHue(hue, '5');
+});
+
+
+
+colorPicker6.addEventListener('input', () => {
+  currentColor6 = document.getElementById('color6').value;
+  document.querySelector('.column6 label').style.backgroundColor = currentColor6;    
+});
+
+colorPicker6.addEventListener('change', () => {
+let hue = hexToHue(currentColor6);
+shadeHue(hue, '6');
+});
+
+
+
+colorPicker7.addEventListener('input', () => {
+  currentColor7 = document.getElementById('color7').value;
+  document.querySelector('.column7 label').style.backgroundColor = currentColor7;
+  console.log('working')  
+});
+
+colorPicker7.addEventListener('change', () => {
+let hue = hexToHue(currentColor7);
+shadeHue(hue, '7');
+});
+
